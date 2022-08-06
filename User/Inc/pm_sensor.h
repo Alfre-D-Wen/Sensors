8e467a76654442be1327e2cec81ab7f0eeb33cfc
AlfreD <alfred-wen@hotmail.com>
2022-08-06 14:35:26 +0800

    New add

---
+++ User/Inc/pm_sensor.h
@@ -0,0 +1,51 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file    pm_sensor.h
+ * @brief   This file contains all the function prototypes for
+ *          the pm_sensor.c file
+ ******************************************************************************
+ */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __PM_SENSOR_H__
+#define __PM_SENSOR_H__
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx.h"
+#include "dma.h"
+#include "usart.h"
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* USER CODE BEGIN Private defines */
+#define SENSOR_ADDRESS huart1
+#define HEAD 0x68
+#define LENGTH 0x01
+#define CMD_READ_DATA 0x04
+#define CMD_START_TRANS_AUTO 0x40
+#define CMD_STOP_TRANS_AUTO 0x20
+#define CS_READ_DATA 0x93
+#define CS_START_TRANS_AUTO 0x47
+#define CS_STOP_TRANS_AUTO 0x77
+#define PM_OK 0xA5A5
+#define PM_FAIL 0X9696
+    /* USER CODE END Private defines */
+
+    /* USER CODE BEGIN Prototypes */
+    void PM_Read_Data(void);           //读取微粒测量结果
+    void PM_Stop_Transmit_Auto(void);  //传感器停止自动发送
+    void PM_Start_Transmit_Auto(void); //传感器开启自动发送
+    /* USER CODE END Prototypes */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ User/Src/pm_sensor.c
@@ -0,0 +1,39 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file    pm_sensor.c
+ * @brief   This file provides code for configuration
+ *          of PM Sensor. Model:HPMA115S0
+ *******************************************************************************
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include "pm_sensor.h"
+
+uint8_t PM_READ_DATA_COMMAND[4] = {HEAD, LENGTH, CMD_READ_DATA, CS_READ_DATA};
+uint8_t PM_START_TRANS_AUTO_COMMAND[4] = {HEAD, LENGTH, CMD_READ_DATA, CS_READ_DATA};
+uint8_t PM_STOP_TRANS_AUTO_COMMAND[4] = {HEAD, LENGTH, CMD_STOP_TRANS_AUTO, CS_STOP_TRANS_AUTO};
+uint8_t PM_RX_DATA_BUFFER[8];
+uint8_t PM_RX_RESPOND_BUFFER[2];
+uint16_t PM_2_5_DATA;
+
+void PM_Read_Data(void) //读取微粒测量结果
+{
+    uint8_t pm_25_bit1 = PM_RX_DATA_BUFFER[3];
+    uint8_t pm_25_bit0 = PM_RX_DATA_BUFFER[4];
+    HAL_UART_Transmit(&SENSOR_ADDRESS, PM_READ_DATA_COMMAND, 4, 1000);
+    HAL_UART_Receive_DMA(&SENSOR_ADDRESS, PM_RX_DATA_BUFFER, 8);
+    PM_2_5_DATA = pm_25_bit1 * 256 + pm_25_bit0;
+}
+
+void PM_Stop_Transmit_Auto(void) //传感器停止自动发送
+{
+    HAL_UART_Transmit(&SENSOR_ADDRESS, PM_STOP_TRANS_AUTO_COMMAND, 4, 1000);
+    HAL_UART_Receive_DMA(&SENSOR_ADDRESS, PM_RX_RESPOND_BUFFER, 2);
+}
+
+void PM_Start_Transmit_Auto(void) //传感器开启自动发送
+{
+    HAL_UART_Transmit(&SENSOR_ADDRESS, PM_START_TRANS_AUTO_COMMAND, 4, 1000);
+    HAL_UART_Receive_DMA(&SENSOR_ADDRESS, PM_RX_RESPOND_BUFFER, 2);
+}
